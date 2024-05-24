8aaa830e6807a516b05753628184cbfbd46b2998
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-05-24 07:27:09 -0300

    Update

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { PostagemModule } from './postagem/postagem.module';
+import { TemaModule } from './tema/tema.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: DevService,
+      imports: [ConfigModule],
+    }),
+    PostagemModule,
+    TemaModule,
+    AuthModule,
+    UsuarioModule
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule { }
---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: '5d02615b5f2924fd6af47e000bed9a4af334d8473595c0fb16fbc6163770cca4',
+};
